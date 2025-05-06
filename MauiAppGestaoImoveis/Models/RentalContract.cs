61be1e22a3d487e054601d2716d660daa207fc0e
Carlos Gabriel Garcia Campos <carlosgabrielbraga035@gmail.com>
2025-05-06 16:23:40 -0300

    refact: classe RentalContract refatorada

---
+++ MauiAppGestaoImoveis/Models/RentalContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppGestaoImoveis.Models
+{
+    public class RentalContract
+    {
+        public Renter Renter { get; set; }
+        public Adress HouseAdress { get; set; }
+        public DateTime StartContract { get; set; }
+        public DateTime EndContract { get; set; }
+        public double RentalValue { get; set; }
+        public ICollection<Bills> Bills { get; set; }
+
+        public RentalContract(Renter renter, Adress houseAdress, DateTime startContract, DateTime endContract, double rentalValue, ICollection<Bills> bills)
+        {
+            Renter = renter;
+            HouseAdress = houseAdress;
+            StartContract = startContract;
+            EndContract = endContract;
+            RentalValue = rentalValue;
+            Bills = bills;
+        }
+    }
+}
